0e32322eb4d58bcf88a37dfb92c2320d16b98c43
Jagadish Patil <pjagadish18@gmail.com>
2026-08-16 16:56:39 +0530

    Model Trainer

---
+++ src/utils.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+import pickle
+
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+from src.logger import logging
+from src.exception import CustomException
+
+def save_object(file_path,obj):
+
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+
+    try:
+        report=dict()
+        for i in  range(len(models)):
+            model=list(models.values())[i]
+
+            # Train Model
+            model.fit(X_train,y_train)
+
+            # Predict Training data
+            # y_train_pred=model.predict(X_train) not required
+
+            # Predict Testing data
+            y_test_pred=model.predict(X_test)
+
+            # Get R2 score for Train and Test data
+            # train_model_score=r2_score(y_train,y_train_pred) mot required
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.error("Exception Occurred durin Model Training")
+        raise CustomException(e, sys)
+
